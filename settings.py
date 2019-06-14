191e19e5dbfa6ba4f3ae0a869ce8953a2b6fefea
Gaurav Burjwal <gauravburjwal@gmail.com>
2019-06-14 13:03:53 +0530

    created settings class and added screen dimension and color

---
+++ settings.py
@@ -0,0 +1,10 @@
+# class for the settings of the game
+class Settings():
+    """A class to store all the settings for Alien Invasion game."""
+
+    def __init__(self):
+        """Initialize the game's setings."""
+        # screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (175, 175, 175)
