ea10eca253a42deecc7db4d8bb31b2e777c2a23b
Gaurav Burjwal <gauravburjwal@gmail.com>
2019-06-14 13:44:53 +0530

    changed background color

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ class Settings():
         # screen settings.
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (175, 175, 175)
+        self.bg_color = (230, 230, 230)
