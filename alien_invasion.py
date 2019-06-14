f01fbe49efd09410fdaf4015e91691330880aae8
Gaurav Burjwal <gauravburjwal@gmail.com>
2019-06-14 14:24:06 +0530

    added right movement  by 1 pixel

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,7 +16,7 @@ def run_game():
 
     # start the main loop for the game
     while True:
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,16 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
     """Respond to keypress and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # move ship to the right.
+                ship.rect.centerx += 1
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
