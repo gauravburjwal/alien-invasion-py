4f8964c5299a484d449e9e7caba6172939d71642
Gaurav Burjwal <gauravburjwal@gmail.com>
2019-06-14 15:38:35 +0530

    refactored the check_events function

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,20 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Respond to releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Respond to keypress and mouse events."""
     for event in pygame.event.get():
@@ -9,16 +23,10 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
@@ -28,3 +36,4 @@ def update_screen(ai_settings, screen, ship):
 
     # make the most recently drawn screen visible.
     pygame.display.flip()
+    
